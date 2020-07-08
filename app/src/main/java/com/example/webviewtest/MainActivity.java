cabd10c697665d9ab99dfccd7fcbd5a8bb171921
k0r4te <k0r4te@gmail.com>
2020-07-08 19:03:48 +0900

    수정

--- app/src/main/java/com/example/webviewtest/MainActivity.java
+++ app/src/main/java/com/example/webviewtest/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.webviewtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.webkit.DownloadListener;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -17,11 +22,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mWebView = (WebView) findViewById(R.id.webView);
+        mWebView = findViewById(R.id.webView);
 
         mWebView.setWebViewClient(new WebViewClient()); // 클릭시 새창 안뜨게
         mWebSettings = mWebView.getSettings(); //세부 세팅 등록
-        mWebSettings.setJavaScriptEnabled(true); // 웹페이지 자바스클비트 허용 여부
+        mWebSettings.setJavaScriptEnabled(true); // 웹페이지 자바스크립트 허용 여부
         mWebSettings.setSupportMultipleWindows(false); // 새창 띄우기 허용 여부
         mWebSettings.setJavaScriptCanOpenWindowsAutomatically(false); // 자바스크립트 새창 띄우기(멀티뷰) 허용 여부
         mWebSettings.setLoadWithOverviewMode(true); // 메타태그 허용 여부
@@ -32,7 +37,8 @@ public class MainActivity extends AppCompatActivity {
         mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용 여부
         mWebSettings.setDomStorageEnabled(true); // 로컬저장소 허용 여부
 
-        mWebView.loadUrl("https://544fcf0f.ngrok.io"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
+        mWebView.loadUrl("https://66e13657.ngrok.io/"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
+
     }
     @Override
     public void onBackPressed() {
@@ -47,6 +53,5 @@ public class MainActivity extends AppCompatActivity {
             lastTimeBackPressed = System.currentTimeMillis();
             Toast.makeText(this,"'뒤로' 버튼을 한 번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
         }
-
     }
 }
